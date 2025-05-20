fb85ac133b1112a19925b24e75124c5faa5d445a
Selim Beltrán <jarbeltr@espol.edu.ec>
2025-05-20 12:55:34 -0500

    v3

---
+++ taller.java
@@ -0,0 +1,17 @@
+public class taller {
+    public static void main(String[] args) {
+        aClass<? extends String> me = new aClass<String>("Hello");
+        me.printar();
+    }
+}
+
+class aClass<T>{
+    private T message;
+    public aClass(T hello){
+        this.message = hello;
+    }
+
+    public void printar(){
+        System.out.println(message);
+    }
+}
